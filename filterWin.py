5c188cb35ad3525d91446db483b6502d4b30741e
Kristyna Zemlova <42995984+zemlovak@users.noreply.github.com>
2018-11-20 21:53:54 +0100

    Add files via upload

--- filterWin.py
+++ filterWin.py
@@ -10,6 +10,7 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 from matplotlib.figure import Figure
 import csv
 from scipy import signal
+import pandas as pd
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -20,29 +21,33 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 # from mainWin import my_channel
 # import random
 
-class Filtrace(QMainWindow):
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.left = 10
         self.top = 10
         self.title = 'XRD data filtering'
-        self.width = 720
+        self.width = 660
         self.height = 550
 
-        self.m = PlotCanvas(self, width=7.05, height=4)
+        self.m = PlotCanvas(self, width=6.45, height=4)
         self.m.move(7, 7)
         self.tools = NavigationToolbar(self.m, self)
-        self.tools.move(7, 513)
-        self.tools.resize(417, 30)
+        self.tools.move(10, 513)
+        self.tools.resize(430, 30)
 
         self.label0 = QLabel('Select data filter and its parameters', self)
-        self.labelx = QLabel('Export file', self)
+        self.labelx = QLabel('Export to file', self)
         self.rad1 = QRadioButton("&Zero-phase")
         self.rad2 = QRadioButton("&Savitzky-Golay")
         self.rad3 = QRadioButton("&Median")
         self.rad4 = QRadioButton("&Exponential smoothing")
         self.slide1 = QSlider(Qt.Horizontal)
         self.slide2 = QSlider(Qt.Horizontal)
+
+        self.b1 = QPushButton('Save data as .csv', self)
+        self.b2 = QPushButton('Save data as .xls', self)
+        self.b3 = QPushButton('Save chart as .png', self)
 
         self.data = np.genfromtxt('testovaci.dat')
 
@@ -53,7 +58,7 @@ class Filtrace(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setMaximumSize(720, 550)
+        self.setMaximumSize(660, 550)
 
         self.layout1 = QHBoxLayout()
         self.layout2 = QHBoxLayout()
@@ -62,9 +67,9 @@ class Filtrace(QMainWindow):
         widget2 = QWidget(self)
         widget1.setLayout(self.layout1)
         widget2.setLayout(self.layout2)
-        widget1.resize(420, 35)
+        widget1.resize(440, 35)
         widget1.move(10, 440)
-        widget2.resize(420, 35)
+        widget2.resize(440, 35)
         widget2.move(10, 468)
 
         self.layout1.addWidget(self.rad1)
@@ -84,7 +89,7 @@ class Filtrace(QMainWindow):
         self.label0.resize(250, 26)
         self.label0.setFont(QFont("Sans Serif", 11))
 
-        self.labelx.move(545, 412)
+        self.labelx.move(530, 412)
         self.labelx.resize(150, 26)
         self.labelx.setFont(QFont("Sans Serif", 11))
 
@@ -109,19 +114,48 @@ class Filtrace(QMainWindow):
         self.slide2.setMaximumWidth(110)
         self.slide2.setMinimum(1)
         self.slide2.setMaximum(100)
-        self.slide2.setToolTip('Alpha parameter')
+        self.slide2.setToolTip('Parameter alpha')
         self.slide2.setSingleStep(1)
         self.slide2.setTickInterval(10)
         self.slide2.setTickPosition(QSlider.TicksBelow)
         self.slide2.setFocusPolicy(Qt.StrongFocus)
 
+        ###button1 - .csv
+        self.b1.setToolTip('Click to save data to .csv file')
+        self.b1.move(510, 450)
+        self.b1.resize(120, 27)
+        self.b1.clicked.connect(lambda: self.b1_fcn(self.m.dat))
+        ### button2 - .xls
+        self.b2.setToolTip('Click to save data to .xls file')
+        self.b2.move(510, 479)
+        self.b2.resize(120, 27)
+        #button 3 - .png
+        self.b3.setToolTip('Click to save chart as .png')
+        self.b3.move(510, 508)
+        self.b3.resize(120, 27)
+        self.b3.clicked.connect(lambda: self.b3_fcn(self.m.figure))
+
         self.m.plot(self.data, self.my_channel)
 
         self.show()
 
+    def b1_fcn(self, cisla):
+        self.m.dat = cisla
+        np.savetxt("filename.csv", cisla, delimiter=",")
+        print(len(cisla))
+
+    def b2_fcn(self, cisla):
+        self.m.dat = cisla
+        cisla = pd.DataFrame(cisla)
+        cisla.to_excel('filename.xlsx', index=False)
+
+    def b3_fcn(self, fig):
+        self.m.figure = fig
+        fig.savefig('filename.png')
+
 
 class PlotCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=7.05, height=4, dpi=100):
+    def __init__(self, parent=None, width=6.45, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
 
 
@@ -180,6 +214,8 @@ class PlotCanvas(FigureCanvas):
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Intensity ()')
 
+        self.dat = f
+
         self.draw()
 
     def rad2click(self, data_plt, channel):
@@ -205,35 +241,39 @@ class PlotCanvas(FigureCanvas):
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Intensity ()')
 
+        self.dat = sg
+
         self.draw()
 
-    # def rad3click(self, data_plt, channel):
-    #     # median filter
-    #     self.figure.clear()
-    #     self.my_channel = channel
-    #     self.data = data_plt
-    #
-    #     ax = self.figure.add_subplot(111)
-    #     ax.autoscale(enable=True, axis='x', tight=bool)
-    #
-    #     row = data_plt.shape[0]
-    #     col = data_plt.shape[1]
-    #     data_plt = data_plt[:, 7:col]
-    #
-    #     r = data_plt[:, 149]
-    #     s = np.arange(0, row, 1)
-    #
-    #     mf = signal.medfilt(r, win)
-    #     ax.plot(s, r, linewidth=0.5, c=[0.80, 0, 0.2])
-    #     ax.plot(s, mf, linewidth=2.0, c='g')
-    #
-    #     ax.set_xlabel('Time (s)')
-    #     ax.set_ylabel('Intensity ()')
-    #
-    #     self.draw()
+    def rad3click(self, data_plt, channel):
+        # median filter
+        self.figure.clear()
+        self.my_channel = channel
+        self.data = data_plt
+
+        ax = self.figure.add_subplot(111)
+        ax.autoscale(enable=True, axis='x', tight=bool)
+
+        row = data_plt.shape[0]
+        col = data_plt.shape[1]
+        data_plt = data_plt[:, 7:col]
+
+        r = data_plt[:, 149]
+        s = np.arange(0, row, 1)
+
+        mf = signal.medfilt(r, win)
+        ax.plot(s, r, linewidth=0.5, c=[0.80, 0, 0.2])
+        ax.plot(s, mf, linewidth=2.0, c='g')
+
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Intensity ()')
+
+        self.dat = mf
+
+        self.draw()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Filtrace()
+    ex = App()
     sys.exit(app.exec_())
