a82e2352f216e0f7d6888e6bf323a21b9998c82c
vrbaj <vrbik.vrbik@gmail.com>
2018-11-21 11:31:08 +0100

    minor changes

--- jakNaFiltraci.py
+++ jakNaFiltraci.py
@@ -2,8 +2,8 @@ import numpy as np
 
 data_x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 data_filtered = np.zeros(data_x.shape)
-alpha = 2 # koeficient u filtrovanych dat
-beta = 1 # koeficient u nefiltrovanych dat
+alpha = 2  # koeficient u filtrovanych dat
+beta = 1  # koeficient u nefiltrovanych dat
 # vyfiltrovana data (k) = beta * nefiltrovana data(k) + alpha * vyfiltrovana data(k-1)
 
 for idx, x in np.ndenumerate(data_x):
@@ -11,6 +11,6 @@ for idx, x in np.ndenumerate(data_x):
         data_filtered[0] = x
     else:
         data_filtered[idx[0]] = beta * x + alpha * data_filtered[idx[0] - 1]
-        print("beta * ",x,"+ alpha * ", data_filtered[idx[0] - 1])
+        print("beta * ", x, "+ alpha * ", data_filtered[idx[0] - 1])
 print(data_x)
 print(data_filtered)
